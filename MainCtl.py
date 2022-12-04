21c288eab9f19d20ee98319a3715ea4ab8b2828b
hebeallrusty <hebeallrusty@gmail.com>
2022-12-04 14:13:38 +0000

    added in bme280 prototype code

--- MainCtl.py
+++ MainCtl.py
@@ -1,10 +1,18 @@
 
 print("Loading modules")
+# standard libraries
 import datetime as dt
 import time as tm
+import configparser
+
+# installed libraries
+import bme280
+import smbus2
+
+# custom libraries
 from Sun import Sun
 from Moon import Moon
-import configparser
+
 
 
 print("Initilising")
@@ -19,9 +27,19 @@ Config.read(CONFIG_FILE)
 # Get location for sunrise / set times
 LOCATION = [float(Config.get('LOCATION','latitude')),float(Config.get('LOCATION','longitude'))]
 
-print(LOCATION)
+# get the i2c port
+i2c_port = int(Config.get('I2C','Port'))
+
+# get bme280 sensor address; address needs to be base16 integer but is stored as string
+bme280_address = int(Config.get('I2C','BME280'),16)
 
 
+# initialise BME280 sensor
+bme280_bus = smbus2.SMBus(i2c_port)
+bme280_calibration_params = bme280.load_calibration_params(bme280_bus,bme280_address)
+
+
+# start a loop update everything
 while True:
     
     # get time and store it to be used throughout the loop to prevent the rare potential where the date changes by the time the loop completes
@@ -33,9 +51,11 @@ while True:
     m = Moon(NOW)
     sunrise = today + s.Rise()['Official']
     sunset = today + s.Set()['Official']
-    print("SunRise: ",sunrise.hour,":",sunrise.minute,"; SunSet: ",sunset.hour,":",sunset.minute)
+    print("SunRise: ",f'{sunrise.hour:02d}',":",f'{sunrise.minute:02d}',"; SunSet: ",f'{sunset.hour:02d}',":",f'{sunset.minute:02d}')
     print("Moon: ",m.MoonPhase())
-    print(NOW.strftime('%I:%M:%S %a %d-%m-%y'))
+    bme280_data = bme280.sample(bme280_bus,bme280_address,bme280_calibration_params)
+    print("Temp: ",round(bme280_data.temperature,1), "deg Humidity: ", round(bme280_data.humidity,0), "% Pressure: ",round(bme280_data.pressure,0))
+    print(NOW.strftime('%H:%M:%S %a %d-%m-%y'))
     
     
 
