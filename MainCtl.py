9a92788ac95f16008504f86aa8e409054a21c970
hebeallrusty <hebeallrusty@gmail.com>
2023-03-18 16:13:57 +0000

    replaced 7-seg code with that largely based on the my PicoClock code, make time more sychronous with clock ticks rather than just writing to the display every 0.1s

--- MainCtl.py
+++ MainCtl.py
@@ -5,39 +5,18 @@ import datetime as dt
 import time as tm
 import configparser
 
+
 # installed libraries
 import bme280
 import smbus2
+#import timed_count
 
 # custom libraries
 from Sun import Sun
 from Moon import Moon
 from Season import Season
+from Seg import disp, add_dot
 
-# thrown in 7 seg stuff
-# installed blinka and adafruit circuit python ht16k33 module - add to requirements
-#import board
-#from adafruit_ht16k33.segments import Seg7x4
-#from time import sleep
-
-#i2c = board.I2C()
-#display = Seg7x4(i2c,address = 0x70)
-#display.brightness = 0.1
-
-## end of thrown in 7 seg stuff
-
-# added 7 seg matrix method
-#import board
-#from adafruit_ht16k33 import matrix
-
-#i2c = board.I2C()
-#
-#matrix = matrix.Matrix16x8(i2c,address = 0x70)
-
-#matrix.fill(0)
-from Seg import display
-
-########## end
 
 print("Initilising")
 # initialisation of loop
@@ -62,34 +41,73 @@ bme280_address = int(Config.get('I2C','BME280'),16)
 bme280_bus = smbus2.SMBus(i2c_port)
 bme280_calibration_params = bme280.load_calibration_params(bme280_bus,bme280_address)
 
+# get time and store it to be used throughout the loop to prevent the rare potential where the date changes by the time the loop completes
+NOW = dt.datetime.now()
 
+#t_0 = 0
+#t_1 = 0
+
+
+ 
 # start a loop update everything
 while True:
     
-    # get time and store it to be used throughout the loop to prevent the rare potential where the date changes by the time the loop completes
-    NOW = dt.datetime.now()
-    today = dt.datetime(NOW.year,NOW.month,NOW.day,0,0,0)
+#    t_1 = tm.time()
 
+#    print("elapsed time: ",t_1 - t_0)
 
-    s = Sun(NOW,LOCATION,0)
-    m = Moon(NOW)
-    sunrise = today + s.Rise()['Official']
-    sunset = today + s.Set()['Official']
-    print("SunRise: ",f'{sunrise.hour:02d}',":",f'{sunrise.minute:02d}',"; SunSet: ",f'{sunset.hour:02d}',":",f'{sunset.minute:02d}')
-    print("Moon: ",m.MoonPhase())
-    bme280_data = bme280.sample(bme280_bus,bme280_address,bme280_calibration_params)
-    print("Temp: ",round(bme280_data.temperature,1), "deg Humidity: ", round(bme280_data.humidity,0), "% Pressure: ",round(bme280_data.pressure,0))
-    print(Season(NOW))
-    print(NOW.strftime('%H:%M:%S %a %d-%m-%y'))
+    while NOW.second == dt.datetime.now().second:
+        tm.sleep(0.1)
+#    t_0 = tm.time()
     
-    #tempdigits = str(round(bme280_data.temperature,0))
-    #for i in range(0,2):
-    #    display[i]=str(tempdigits[i])
-    display(int(NOW.hour),(0,0))
-    display(int(NOW.minute),(1,0))
-    display(int(NOW.second),(2,0))
-    display(int(round(bme280_data.temperature,0)),(4,0))
-    display(int(round(bme280_data.humidity,0)),(5,0))
 
 
-    tm.sleep(0.1)
+    if NOW.minute == 0:
+        # only run the once per hour
+        if NOW.second == 0:
+            print("Sun / moon calcs")    
+            today = dt.datetime(NOW.year,NOW.month,NOW.day,0,0,0)
+            s = Sun(NOW,LOCATION,0)
+            m = Moon(NOW)
+            sunrise = today + s.Rise()['Official']
+            sunset = today + s.Set()['Official']
+            #print("SunRise: ",f'{sunrise.hour:02d}',":",f'{sunrise.minute:02d}',"; SunSet: ",f'{sunset.hour:02d}',":",f'{sunset.minute:02d}')
+            #print("Moon: ",m.MoonPhase())
+            #print(Season(NOW))
+
+    
+    # get sensor data
+    bme280_data = bme280.sample(bme280_bus,bme280_address,bme280_calibration_params)
+    #print("Temp: ",round(bme280_data.temperature,1), "deg Humidity: ", round(bme280_data.humidity,0), "% Pressure: ",round(bme280_data.pressure,0))
+
+    #print(NOW.strftime('%H:%M:%S %a %d-%m-%y'))
+    
+
+    # to display it, they need to be in a zero-padded list so that the digits can be iterated over
+    bme_t = [int(x) for x in f"{int(round(bme280_data.temperature,0)):02d}"]
+    bme_h = [int(x) for x in f"{int(round(bme280_data.humidity,0)):02d}"]
+
+    # update time variable
+    NOW = dt.datetime.now()
+
+    hr = [int(x) for x in f"{NOW.hour:02d}"]
+    mn = [int(x) for x in f"{NOW.minute:02d}"]
+    sc = [int(x) for x in f"{NOW.second:02d}"]
+
+    # display loop
+    for i in range(0,2):
+        # Sensor
+        disp(bme_h[i],10+i)
+        disp(bme_t[i],8+i)
+
+        # Time
+        disp(hr[i], 0+i)
+        disp(mn[i], 2+i)
+        disp(sc[i], 4+i)
+
+    # add dots to the display
+    #add_dot(1)
+    #add_dot(3)       
+
+
+
--- Seg.py
+++ Seg.py
@@ -1,5 +1,5 @@
 import board
-#from adafruit_ht16k33.segments import Seg7x4
+
 from adafruit_ht16k33 import matrix
 from time import sleep
 
@@ -8,141 +8,80 @@ i2c = board.I2C()
 matrix = matrix.Matrix16x8(i2c,address = 0x70)
 
 matrix.fill(0)
-matrix.brightness = 0.1
-
-
-# something isn't right about this code - it works, but not in the way that it should, or that is expected based on the wiring. The matrix may be the wrong way around!!'
+matrix.brightness = 1
 
 def glyph(g):
     # return an tuple with the digits to set as on for the glyph it represents. Not to be called directly, but through a function. It won't explode, but it'll only default producing glyphs that'll work on the first cluster on a cathode, which can hold 2nr'
-    if g == 1:
-        return (1,2)
-    elif g == 2:
-        return (0,1,6,4,3)
-    elif g == 3:
-        return (0,1,6,2,3)
-    elif g == 4:
-        return (5,6,1,2)
-    elif g == 5:
-        return (0,5,6,2,3)
-    elif g == 6:
-        return (0,5,4,3,2,6)
-    elif g == 7:
-        return (0,1,2)
-    elif g == 8:
-        return (0,1,2,3,4,5,6)
-    elif g == 9:
-        return (0,5,6,1,2,3)
-    elif g == 0:
-        return (0,1,2,3,4,5)
-    elif g == 11:
-        return (7,)
-    else:
-        return (0,5,6,4,3)
-
-def antiglyph(g):
-    # for blanking out segments that aren't part of the glyph needed so that whole display doesn't need blanking - eliminates blinking where the whole display updates'
-    if g == 1:
-        return (0,3,4,5,6,7)
-    elif g == 2:
-        return (2,5,7)
-    elif g == 3:
-        return (4,5,7)
-    elif g == 4:
-        return (0,4,3,7)
-    elif g == 5:
-        return (1,4,7)
-    elif g == 6:
-        return (1,7)
-    elif g == 7:
-        return (5,6,4,3,7)
-    elif g == 8:
-        return (7,)
-    elif g == 9:
-        return (4,7)
-    elif g == 0:
-        return (6,7)
-    elif g == 11:
-        return (1,2,3,4,5,6)
-    else:
-        return (0,5,6,4,3)
+    # returns [glyph segments, antiglyph segments for blanking]
+    #
+    # glyphs are 0 for the top seg going clockwise with 7 as the decimal point
     
+    if g == 1: # 1
+        return [(1,2),(0,3,4,5,6,7)]
+    elif g == 2: # 2
+        return [(0,1,6,4,3),(2,5,7)]
+    elif g == 3: # 3
+        return [(0,1,6,2,3),(4,5,7)]
+    elif g == 4: # 4
+        return [(5,6,1,2),(0,4,3,7)]
+    elif g == 5: # 5
+        return [(0,5,6,2,3),(1,4,7)]
+    elif g == 6: # 6
+        return [(0,5,4,3,2,6),(1,7)]
+    elif g == 7: # 7
+        return [(0,1,2),(5,6,4,3,7)]
+    elif g == 8: # 8
+        return [(0,1,2,3,4,5,6),(7,)]
+    elif g == 9: # 9
+        return [(0,5,6,1,2,3),(4,7)]
+    elif g == 0: # 0
+        return [(0,1,2,3,4,5),(6,7)]
+    elif g == 11: # .
+        return [(7,),(1,2,3,4,5,6)]
+    elif g == 12: # [blank / space]
+        return [(),(0,1,2,3,4,5,6,7)]
+    elif g == 13: # b
+        return [(5,4,3,2,6),(0,1,7)]
+    elif g == 14: # t
+        return [(5,4,3,6),(0,1,2,7)]
+    elif g == 15: # I
+        return [(4,5),(0,1,2,3,6,7)]
+    elif g == 16: # n
+        return [(2,4,6),(0,1,3,5,7)]
+    elif g == 17: # s
+        return [(0,5,6,2,3),(1,4,7)]
+    elif g == 18: # d
+        return [(1,2,3,4,6),(0,5,7)]
+    elif g == 19: # c
+        return [(0,3,4,5),(1,2,6,7)]
+    elif g == 20: # e
+        return [(0,5,6,1,4,3),(2,7)]
+    elif g == 21: # r
+        return [(4,6),(0,1,2,3,5,7)]
+    elif g == 22: # o
+        return [(6,2,3,4),(0,1,5,7)]
 
-def column(c,g):
-    # each cathode on the ht16k33 can hold 2nr clusters, (one on A0 - A7;the second on A8 A15. All glyphs are for A0 - A7; this shifts them over to A8 - A15)
-    # alter each glyph to suit column 1 or column 2 (A0 - A7 or A8 - A15)
-    # c is the cluster - 0 for A0 - A7; 1 for A8 - A15 AKA the column
-    # g is the actual glyph to display
+    else: # return an E
+        return [(0,5,6,4,3),(1,2,7)]
 
-    #print(c,":",g)
-
-    # get the glyph to display
-    t = glyph(g)
-    u = antiglyph(g)
-
-    #print([t,u])
-
-    # glyph needs to be transposed into the correct cluster
-    if c == 0:
-        return [t,u]
-    elif c == 1:
-        # each glyph just needs shifted by 8 rows for the second cluster on each cathode
-        #print([tuple([z + 8 for z in t]), tuple([x + 8 for x in u])])
-        return [tuple([z + 8 for z in t]), tuple([x + 8 for x in u])]
-    else:
-        return [t,u]
-
-def display(n,coords):
-    # n is a 2 digit number as displays are 2 digits. Zero padding will be taken care of.
-    # coords should be in the form (r,c) as noted below
-    # r is cathode tuple (internal wiring of display has each 2 digit with 2 cathodes which select the left or right segment to display on; each bar of the anode are wired together i.e. bar a for both the left and right digit share one wire) AKA the ROW (0 - 4)
-    # c is the anode i.e. the left or right cluster for each cathode AKA the COLUMN (0 - 1)
-
-    (r,c) = coords
-    #print(r)
-    #print(c)
-
-    # pad the number with zero's and convert to string
-    npad = f'{n:02d}'
+def disp(d, bank):
+    # plot a digit and display it on the bank noted
     
-    #empty display - THIS MAY NEED REMOVING WHEN EXTRA DIGITS ARE ADDDED
-    #matrix.fill(0)    
-    
-    #print(npad)
-    # iterate over the digits, get the glyph, correct it and then allow it to be displayed
-    for i in range(0,2):
-        # get the corrected glyph depending on the column
-        g = column(c,int(npad[i]))
-        # iterate over the glyph bars and display it
-        for j in g[0]:
-            matrix[2 * r + i, j] = 1
-        # blank out those glyph bars that aren't relevant with the antiglyph'
-        for j in g[1]:
-            matrix[2 * r + i, j] = 0
-    # small hack to make the hour / minute decimal points appear    
-    matrix[1,7] =1
-    matrix[3,7] = 1
-    
+    # get the glyphs and anti-glyphs for digit g
+    g,a = glyph(d)
 
+    #print(g,a)
+    # glyphs to light
+    for i in g:
+        #print("on:",i)
+        matrix[bank,i] = 1
+        
+    # glyphs to blank off
+    for i in a:
+        #print("off:",i)
+        matrix[bank,i] = 0
 
-#display(5,(5,0))
-#for i in range(0,8):
-#    for j in range(0,17):
-#        matrix[j,i]=1
-#        sleep(0.1)
-
-#
-##
-# matrix[c,a] where c = cathode (digit); a = anode (segment)
-
-#matrix[8,0]=1
-#matrix[8,1]=1
-
-#while True:
-#    display(64,(0,0))
-#    #matrix[0,6]=0
-#    sleep(0.5)
-
-
+def add_dot(bank):
+    matrix[bank,7] = 1
 
 
